a60f7e82b1a86e5b7f4221bd9c45ca9dc232dcbe
PAULFONTENEAU <156661748+PAULFONTENEAU@users.noreply.github.com>
2025-12-13 17:11:16 +0100

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -69,7 +69,7 @@ export const siteConfig = {
   // Qui sommes nous ? 
   // ============================================
   features: {
-    title: 'Qui sommes nous ?',
+    title: '3435 ?',
     subtitle:
       "Deux jeunes passionnées de triathlon, en quête d’une préparation plus intelligente et accessible.",
     items: [
